384f708497afde3fbd707603fea315a3c6d89eab
siddontang <siddontang@gmail.com>
2015-12-22 14:28:09 +0800

    proto: response supports codec itself.

--- proto/proto.go
+++ proto/proto.go
@@ -1,5 +1,12 @@
 package proto
 
+import (
+	"encoding/binary"
+	"io"
+
+	"github.com/juju/errors"
+)
+
 type RequestHeader struct {
 }
 
@@ -10,4 +17,24 @@ type Timestamp struct {
 
 type Response struct {
 	Timestamp
+}
+
+func (res *Response) Encode(w io.Writer) error {
+	var buf [16]byte
+	binary.BigEndian.PutUint64(buf[0:8], uint64(res.Physical))
+	binary.BigEndian.PutUint64(buf[8:16], uint64(res.Logical))
+	_, err := w.Write(buf[0:16])
+	return errors.Trace(err)
+}
+
+func (res *Response) Decode(r io.Reader) error {
+	var buf [16]byte
+	_, err := io.ReadFull(r, buf[0:16])
+	if err != nil {
+		return errors.Trace(err)
+	}
+
+	res.Physical = int64(binary.BigEndian.Uint64(buf[0:8]))
+	res.Logical = int64(binary.BigEndian.Uint64(buf[8:16]))
+	return nil
 }
